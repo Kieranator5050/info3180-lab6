23fa5a3f2d3f0a8e8d1ac4eef75d93b2061eac9c
Kieranator5050 <thejugg63@gmail.com>
2022-04-03 20:23:36 -0500

    created search functionality

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -2,9 +2,30 @@
 export default {
     data() {
       return {
-          articles : []
+          articles : [],
+          searchTerm : ''
       };        
     },
+
+    methods: {
+        searchNews() {
+            let self = this;
+            fetch('https://newsapi.org/v2/everything?q='+
+            self.searchTerm + '&language=en', {
+                headers: {
+                'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`,
+                }
+            })
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(data) {
+                    console.log(data);
+                    self.articles = data.articles;
+                });
+        }
+    },
+
     created() {
         
         let self = this;
@@ -26,6 +47,17 @@ export default {
 </script>
 
 <template>
+
+<form @submit.prevent="searchNews" class="d-flex flex-column justify-content-center">
+    <div class="input-group mx-sm-3 mb-2">
+        <label class="visually-hidden" for="search">Search</label>
+        <input type="search" name="search" v-model="searchTerm"
+        id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+        <button class="btn btn-primary mb-2">Search</button>
+    </div>
+    <p>You are searching for {{ searchTerm }}</p>
+</form>
+
 
 <div class="cardDisplay">
   <div v-for="article in articles" class="card cardBackground" style="width: 18rem;">
