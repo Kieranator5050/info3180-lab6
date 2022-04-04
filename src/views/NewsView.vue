2cc6387ab019e4ad0b6a5ba958e3350eb8181f09
Kieranator5050 <thejugg63@gmail.com>
2022-04-03 20:45:35 -0500

    News route added

---
+++ src/views/NewsView.vue
@@ -0,0 +1,23 @@
+<script>
+
+import NewsList from '@/components/NewsList.vue'
+
+export default {
+    data() {
+        return {};
+    },
+    components: { NewsList }
+};
+</script>
+
+<template>
+    <div class="news container">
+      <div class="text-center">
+        <h2>News</h2>
+        <NewsList />
+      </div>
+    </div>
+</template>
+
+<style>
+</style>
